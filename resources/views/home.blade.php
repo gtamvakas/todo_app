871e32fc9d35b4531aeffd32d7cda3d415c2849d
Giorgos <getamvakas@york.citycollege.eu>
2021-12-22 18:17:38 +0200

    Add register link in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,15 @@
         <form method="POST" action="/login">
             @csrf
             <p class="text-center mb-10 text-2xl"> Login </p>
-            <x-slot name="logo"> <img src='{{ asset('/images/logo_black.png') }}'  class="w-16 md:w-32 lg:w-48" alt="logo"> </x-slot>
+            <x-slot name="logo"> <img src='{{ asset('/images/logo_black.png') }}'  class="w-48 md:w-45 lg:w-48" alt="logo"> </x-slot>
             <x-label for="email" class="mt-5">Email</x-label>
             <x-input type="email" name="email" required size="35" value="{{ old('email') }}"></x-input>
             <x-label for="password" class="mt-5">Password</x-label>
             <x-input type="password" name="password" required size="35"></x-input>
             <x-button class="mt-5"> Login! </x-button>
+            <a href="#" class="ml-56" >Don't have an account?</a>
             <x-auth-validation-errors></x-auth-validation-errors>
         </form>
     </x-auth-card>
+
 </x-guest-layout>
