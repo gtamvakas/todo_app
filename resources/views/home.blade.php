0d50dac481e42c9f79dfeffe0a7250a04a012834
Giorgos <getamvakas@york.citycollege.eu>
2021-12-22 18:01:00 +0200

    Change input fields size in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,9 @@
             <p class="text-center mb-10 text-2xl"> Login </p>
             <x-slot name="logo"> <img src='{{ asset('/images/logo_black.png') }}'  class="w-16 md:w-32 lg:w-48" alt="logo"> </x-slot>
             <x-label for="email" class="mt-5">Email</x-label>
-            <x-input type="email" name="email" required value="{{ old('email') }}"></x-input>
+            <x-input type="email" name="email" required size="35" value="{{ old('email') }}"></x-input>
             <x-label for="password" class="mt-5">Password</x-label>
-            <x-input type="password" name="password" required></x-input>
+            <x-input type="password" name="password" required size="35"></x-input>
             <x-button class="mt-5"> Login! </x-button>
             <x-auth-validation-errors></x-auth-validation-errors>
         </form>
