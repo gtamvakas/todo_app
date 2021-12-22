f28eb116c389dfeedd2a394096973a3e380da3af
Giorgos <getamvakas@york.citycollege.eu>
2021-12-22 23:26:03 +0200

    Change CSS of input fields in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
             <p class="text-center mb-10 text-2xl"> Login </p>
             <x-slot name="logo"> <img src='{{ asset('/images/logo_black.png') }}'  class="w-48 md:w-45 lg:w-48" alt="logo"> </x-slot>
             <x-label for="email" class="mt-5">Email</x-label>
-            <x-input type="email" name="email" required size="35" value="{{ old('email') }}"></x-input>
+            <x-input type="email" class="block w-full" name="email" required value="{{ old('email') }}"></x-input>
             <x-label for="password" class="mt-5">Password</x-label>
-            <x-input type="password" name="password" required size="35"></x-input>
-            <x-button class="mt-5"> Login! </x-button>
-            <a href="#" class="ml-56" >Don't have an account?</a>
+            <x-input type="password" class="block w-full" name="password" required></x-input>
+            <x-button class="mt-5"> LOGIN </x-button>
+            <a href="/register" class="ml-56 underline" >Don't have an account?</a>
             <x-auth-validation-errors></x-auth-validation-errors>
         </form>
     </x-auth-card>
