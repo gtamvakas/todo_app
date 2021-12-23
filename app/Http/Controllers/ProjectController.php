143e24e42435a76ab1d16e1a76963e84f90d46b1
Giorgos <getamvakas@york.citycollege.eu>
2021-12-23 20:02:31 +0200

    Update ProjectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index(){
-        return view();
+        return view('projects.index', [
+            'projects'=> Project::latest()->paginate(10)
+        ]);
     }
 }
