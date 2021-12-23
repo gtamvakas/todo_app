3e5896cb7ad6e3405ea027aa1a866f547a735e9f
Giorgos <getamvakas@york.citycollege.eu>
2021-12-23 14:50:36 +0200

    Add user_id as foreign id in projects migration

--- database/migrations/2021_12_23_112218_create_projects_table.php
+++ database/migrations/2021_12_23_112218_create_projects_table.php
@@ -15,6 +15,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id()->unique();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->timestamps();
         });
