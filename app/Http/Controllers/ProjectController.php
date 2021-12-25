8cc5b5a6033361e87de2ae4cd9758221cf742f09
Giorgos <getamvakas@york.citycollege.eu>
2021-12-25 18:18:05 +0200

    Add authorization check in show method to check if a user can view a project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Project;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Str;
 
 class ProjectController extends Controller
 {
@@ -18,9 +20,30 @@ class ProjectController extends Controller
     }
 
     public function show(Project $project){
-
+        $this->authorize('owner',[$project]);
         return view('projects.show', [
             'project' => $project
+        ]);
+    }
+
+    public function create(){
+        return view('projects.create');
+    }
+
+    public function store(){
+
+        Project::create(array_merge($this->validateProject(),[
+            'user_id' => request()->user()->id,
+            'slug' => Str::slug(request()->input('name'))
+        ]));
+
+        return redirect('/projects');
+    }
+
+    public function validateProject(): array
+    {
+        return request()->validate([
+            'name' => ['required','max:255']
         ]);
     }
 }
