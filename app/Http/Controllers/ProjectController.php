d9548ddeafd15fbfead6b399bb42c5fcae622a93
Giorgos <getamvakas@york.citycollege.eu>
2021-12-25 11:56:57 +0200

    Update ProjectController index() to only show posts that belong to logged in user:

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index(){
+
         return view('projects.index', [
-            'projects'=> Project::latest()->paginate(10)
+            'projects'=> Project::latest()
+                ->where('user_id', auth()->id())
+                ->paginate(10)
+        ]);
+    }
+
+    public function show(Project $project){
+
+        return view('projects.show', [
+            'project' => $project
         ]);
     }
 }
