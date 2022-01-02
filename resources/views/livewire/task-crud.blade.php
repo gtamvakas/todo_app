5d68713013e45b7b400e4ad0e53c8ced5b95cc02
Giorgos <getamvakas@york.citycollege.eu>
2022-01-02 19:22:52 +0200

    Fix formatting for screens smaller than 453px

--- resources/views/livewire/task-crud.blade.php
+++ resources/views/livewire/task-crud.blade.php
@@ -12,14 +12,14 @@
             <div class="flex ml-auto" >
                 <div class="mr-5 shrink-0 mt-1" x-data="{open:true}">
                     <button  wire:click="done"  x-show="open" @click="open = false">
-                        <img src="{{ asset('images/done-icon.png') }}" alt="done" class="xxs:w-5 xs:w-5 sm:w-5 md:w-5 lg:w-5 xl:w-5">
+                        <img src="{{ asset('images/done-icon.png') }}" alt="done" class="w-5 xs:w-5 sm:w-5 md:w-5 lg:w-5 xl:w-5">
                     </button>
                     <button  wire:click="undone"  x-show="!open" @click="open = true">
-                        <img src="{{ asset('images/undo-icon.png') }}" alt="done" class="xxs:w-5 xs:w-5 sm:w-5 md:w-5 lg:w-5 xl:w-5">
+                        <img src="{{ asset('images/undo-icon.png') }}" alt="done" class="w-5 xs:w-5 sm:w-5 md:w-5 lg:w-5 xl:w-5">
                     </button>
                 </div>
                 <button class="shrink-0" wire:click="delete">
-                    <img src="{{ asset('images/delete-icon.png') }}" alt="delete" class="xxs:w-5 xs:w-4 sm:w-4 md:w-4 lg:w-4 xl:w-4">
+                    <img src="{{ asset('images/delete-icon.png') }}" alt="delete" class="w-5 xs:w-4 sm:w-4 md:w-4 lg:w-4 xl:w-4">
                 </button>
             </div>
 
